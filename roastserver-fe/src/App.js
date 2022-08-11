888db5c0d18b4e347a7b979e918c4b36699a2ca2
cathos <cathos@startmail.com>
2022-08-11 15:01:27 -0700

    created build to run on server

--- roastserver-fe/src/App.js
+++ roastserver-fe/src/App.js
@@ -1,14 +1,116 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import React, { useState } from "react";
 import TempsAndTimes from "./components/TempsAndTimes";
-const roastData = 1;
+
+const axios = require("axios");
+
+// const backendAddr = "http://localhost:5000";
+const backendAddr = "http://raspberrypi.local:5000";
+
+let initialData = {
+  roast_minutes: 4,
+};
+
+const testData = {
+  bean_temp: 25.7,
+  bt_ror: -0.0,
+  coil_fan_1_rpm: 840,
+  coil_fan_2_rpm: 0,
+  delta_t: 25.5,
+  drum_speed_level: 0,
+  ext_t: -65.2,
+  fan_level: 0,
+  fan_speed: 0,
+  heater_level: 0,
+  ir_bt: 30.9,
+  pcb_temp: 27.5,
+  preheat_temp: 2560,
+  roast_minutes: 48,
+  roast_seconds: 17,
+  roaster_state: "off",
+  voltage: 121,
+};
 
 function App() {
+  async function checkConnection() {
+    try {
+      const result = await axios.get(`${backendAddr}`);
+      console.log(result);
+      return result.data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async function initRoaster() {
+    try {
+      const result = await axios.post(`${backendAddr}/init`, {
+        init: "",
+      });
+      return result.status;
+    } catch (err) {
+      console.error(err);
+      console.log("roaster connection not initialized");
+    }
+  }
+
+  async function getRoasterInfo() {
+    try {
+      const result = await axios.get(`${backendAddr}/info`);
+      return result.data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  // async function getRoasterStatus() {
+  //   try {
+  //     const result = await axios.get(`${backendAddr}/status`);
+  //     console.log(result.data);
+  //     return result.data;
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // }
+  // rewrite this function to use non-async
+  function getRoasterStatus() {
+    axios
+      .get(`${backendAddr}/status`)
+      .then((response) => {
+        console.log(response.data);
+        return response.data;
+      })
+      .catch((error) => {
+        console.log("roasterStatus error: " + error.response);
+      });
+  }
+
+  async function changeRoasterState(button) {
+    try {
+      const result = await axios.post(`${backendAddr}/change`, {
+        button,
+      });
+      return result.data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  // const connectionStatus = checkConnection();
+  // initRoaster();
+  // const roastData = useState(initialData, getRoasterStatus());
+  const roastData = getRoasterStatus();
+  // const roastData = testData;
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h1 className="Title">Roast Server </h1>
+        <nav>{/* <div connection={connectionStatus}></div> */}</nav>
+      </header>
       <main>
-        <TempsAndTimes roaster-data={roastData}></TempsAndTimes>
+        <TempsAndTimes roasterdata={roastData}></TempsAndTimes>
       </main>
     </div>
   );
--- roastserver-fe/src/components/TempsAndTimes.js
+++ roastserver-fe/src/components/TempsAndTimes.js
@@ -2,17 +2,38 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const TempsAndTimes = (props) => {
+  console.log(props);
+  const roasterdata = props.roasterdata;
+  const roastTime = `${roasterdata.roast_minutes}:${roasterdata.roast_seconds}`;
+  // const timeSinceFC =
+  // const devPerc = (timeSinceFC / roastTime)
   return (
     <div className="Temps-and-Times">
-      <h3 className="Roast-Time">Time:{props.roasttime}</h3>
-      <h3 className="IR-Bean-Temp">{props.irbt}</h3>
-      <h3 className="TC-Bean-Temp">{props.tcbt}</h3>
-      <h3 className="BT-ROR">{props.btror}</h3>
-      <h3 className="Delta-T">{props.deltat}</h3>
-      <h3 className="Dev-Percentage">{props.devperc}</h3>
-      <h3 className="Time-Since-FC">{props.timesincefc}</h3>
+      <h3 className="Roast-Time" alt="Roast Time">
+        Time: {roastTime}
+      </h3>
+      <h3 className="IR-Bean-Temp" alt="Infrared Bean Temperature">
+        IR Bean Temp: {roasterdata.ir_bt}
+      </h3>
+      <h3 className="TC-Bean-Temp" alt="Thermocouple Bean Temperature">
+        TC Bean Temp: {roasterdata.bean_temp}
+      </h3>
+      <h3 className="BT-ROR" alt="Bean Temperature Rate of Rise">
+        BT-ROR: {roasterdata.bt_ror}
+      </h3>
+      <h3 className="Delta-T" alt="Delta Temperature">
+        Delta-T: {roasterdata.delta_t}
+      </h3>
+      <h3 className="Dev-Percentage" alt="Development Percent">
+        {/* Development: {roasterdata.devPerc} */}
+      </h3>
+      <h3 className="Time-Since-FC" alt="Time Since First Crack">
+        {/* Time Since FC: {roasterdata.timeSinceFC} */}
+      </h3>
       <section className="Extra-Info">
-        <h4 className="Ext-Temp">{props.exttemp}</h4>
+        <h4 className="Ext-Temp" alt="Exhaust Temperature">
+          {/* Ext Temp: {roasterdata.ext_t} */}
+        </h4>
       </section>
     </div>
   );
